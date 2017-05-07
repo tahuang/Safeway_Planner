6067d95a72d677fcd5ae5dae0d05c06c647341b1
tahuang <tahuang@users.noreply.github.com>
2017-05-07 11:00:12 -0700

    Updated planner with going back up aisles
    Also added reading aisle mapping from text file

--- safeway.py
+++ safeway.py
@@ -4,10 +4,19 @@
 # May 6, 2017
 # Tiffany Huang
 
-num_aisles = 20
-mapping = {'bread' : 7, 'peanut butter' : 4, 'potatoes' : 0, 
-'cheese' : 1, 'bananas' : 0}
+import math
 
+# Number of aisles total
+num_aisles = 20
+
+# Create mapping between items and which aisle they are in from text file.
+mapping = {}
+with open("aisle_mapping.txt") as f:
+    for line in f.read().splitlines():
+       (key, val) = line.split(",")
+       mapping[key] = float(val)
+
+# Read in the shopping list
 input = open('shopping_list.txt', 'r')
 items = input.read().splitlines()
 
@@ -18,13 +27,27 @@ for i in items:
 	else:
 		route.append((i, num_aisles + 1))
 
+# Create the route
 route.sort(key=lambda tup: tup[1])
 
-for i in route:
+# Output the route
+for idx, i in enumerate(route):
+	aisle_name = int(math.floor(i[1]))
 	if (i[1] == num_aisles + 1):
-		print("Unknown:"),
-		print(i[0])
-	else:
-		print(i[1]),
-		print(":"),
-		print(i[0])
+		aisle_name = "Unknown"
+	elif (i[1] == 0):
+		aisle_name = "Produce"
+	elif (i[1] == 2.5):
+		aisle_name = "Meat"
+	print(aisle_name),
+	print(":"),
+	print(i[0])
+
+	# Determine if you go all the way through the aisle or go back up
+	# for the next item.
+	if (idx != len(route) - 1):
+		next_aisle = route[idx+1][1]
+		if (next_aisle % 1 == 0 and math.floor(i[1]) != math.floor(next_aisle) and 
+			next_aisle != num_aisles + 1):
+			print("Go back up Aisle"),
+			print(aisle_name)

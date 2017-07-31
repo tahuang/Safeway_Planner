e0ccdcc8a7a857d5def96c9f708ff4a9e39c9d6a
Tiffany Huang <tahuang08@gmail.com>
2017-07-30 21:09:39 -0700

    Created recipe and meals class.

--- planner_app.py
+++ planner_app.py
@@ -5,21 +5,37 @@
 # Tiffany Huang
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 
-class GroceryList(Widget):
-    cookies = ObjectProperty(None)
+# Builder used to load all the kivy files
+Builder.load_file('planner.kv')
+Builder.load_file('recipe.kv')
+Builder.load_file('meals.kv')
 
-class FoodItem(Widget):
+class GroceryList(Widget):
+	def __init__(self,**kwargs):
+		super(GroceryList,self).__init__(**kwargs)
+
+class Recipes(Widget):
+	def __init__(self,**kwargs):
+		super(Recipes,self).__init__(**kwargs)
+
 	def on_touch_move(self, touch):
 		self.center_x = touch.x
 		self.center_y = touch.y
 
+class Meals(GridLayout):
+	def __init__(self,**kwargs):
+		super(Meals,self).__init__(**kwargs)
+
 class PlannerApp(App):
     def build(self):
+        self.title = "Grocery Planner"
         return GroceryList()
-
 
 if __name__ == '__main__':
     PlannerApp().run()

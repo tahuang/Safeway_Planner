e6903bc1013558de72ae899f6c00b50c616222bc
Tiffany Huang <tahuang08@gmail.com>
2017-08-21 21:41:43 -0700

    planner_app.py: Fixed grid bug, planner.kv: Trying some new things with Scatter

--- planner_app.py
+++ planner_app.py
@@ -7,6 +7,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, NumericProperty, ListProperty, StringProperty
@@ -24,21 +25,6 @@ class GroceryList(BoxLayout):
 class Recipes(Widget):
 	def __init__(self,**kwargs):
 		super(Recipes,self).__init__(**kwargs)
-
-	def on_touch_down(self, touch):
-		if self.collide_point(*touch.pos):
-			touch.grab(self)
-            # do whatever else here
-
-	def on_touch_move(self, touch):
-		if touch.grab_current is self:
-			self.pos = touch.pos
-            # now we only handle moves which we have grabbed
-	
-	def on_touch_up(self, touch):
-		if touch.grab_current is self:
-			touch.ungrab(self)
-            # and finish up here
 
 class Meals(GridLayout):
 	def __init__(self, **kwargs):
@@ -72,14 +58,13 @@ class DrawGrid(Widget):
 			canvas = self.canvas.before
 		width = self.size[0]
 		height = self.size[1]
-		iters = 0
-		grid_width_spacing = width / 7
-		grid_height_spacing = height / 3
+		iters = 1
+		grid_width_spacing = width / 7.0
+		grid_height_spacing = height / 3.0
 		#draw grid interior
 		with canvas:
 			Color(rgba=self.grid_line_color)
 			while width >= grid_width_spacing:
-				
 				Line(width=self.grid_line_width,
 					points=(self.pos[0] + iters*grid_width_spacing, 
 						self.pos[1], 
@@ -87,7 +72,7 @@ class DrawGrid(Widget):
 						self.pos[1] + self.size[1]))
 				width -= grid_width_spacing
 				iters += 1
-			iters = 0
+			iters = 1
 			while height >= grid_height_spacing:
 				Line(width=self.grid_line_width,
 					points=(self.pos[0], 

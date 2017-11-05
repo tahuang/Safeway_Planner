cbe345fef395d5a8a332d7d063a2921c4e80b8f9
Tiffany Huang <tahuang08@gmail.com>
2017-11-04 21:33:47 -0700

    Added shopping area dropdown list

--- planner_app.py
+++ planner_app.py
@@ -12,8 +12,12 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, NumericProperty, ListProperty, StringProperty
 from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.base import runTouchApp
 
 import numpy as np
+import glob
+
 import build_list
 import safeway
 
@@ -26,9 +30,10 @@ class Planner(BoxLayout):
 
 class GetRouteButton(Button):
         meals = ObjectProperty()
+        shopping_area = ObjectProperty()
 
         # Reads text input from user and creates grocery route.
-        def getGroceryRoute(self, meal_list):
+        def getGroceryRoute(self, meal_list, map_file):
                 items = dict()
                 # Parse each meal block for different items
                 for meal in meal_list:
@@ -44,7 +49,7 @@ class GetRouteButton(Button):
                                         items[part] = 1
 
                 build_list.build_list(items)
-                safeway.print_shopping_route()
+                safeway.print_shopping_route(map_file)
 
         def __init__(self,**kwargs):
                 super(GetRouteButton,self).__init__(**kwargs)
@@ -85,6 +90,44 @@ class Recipes(GridLayout):
                 for recipe in recipe_list:
                         item = Button(text=recipe, font_size='14sp')
                         self.add_widget(item)    	
+
+class ShoppingArea(BoxLayout):
+	def __init__(self, **kwargs):
+		super(ShoppingArea,self).__init__(**kwargs)
+
+		dropdown = DropDown()
+		self.text_to_file = {}
+		self.map_file = ''
+		for index, file in enumerate(glob.glob("*.txt")):
+
+			with open(file, 'r') as f:
+				first_line = f.readline().strip()
+				self.text_to_file[first_line] = file
+
+			btn = Button(text=first_line, size_hint_y=None, height=60, background_color=[0, 0, 0.545098, 1])
+
+			# for each button, attach a callback that will call the select() method
+    		# on the dropdown. We'll pass the text of the button as the data of the
+    		# selection.
+			btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+
+			# then add the button inside the dropdown
+			dropdown.add_widget(btn)
+
+		# create a big main button
+		mainbutton = Button(text='Choose shopping area', background_color=[0, 0, 0.545098, 1])
+
+		# show the dropdown menu when the main button is released
+		# note: all the bind() calls pass the instance of the caller (here, the
+		# mainbutton instance) as the first argument of the callback (here,
+		# dropdown.open.).
+		mainbutton.bind(on_release=dropdown.open)
+
+		# one last thing, listen for the selection in the dropdown list and
+		# assign the data to the button text.
+		dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+		dropdown.bind(on_select=lambda instance, x: setattr(self, 'map_file', self.text_to_file[x]))
+		self.add_widget(mainbutton)
 
 class PlannerApp(App):
         def build(self):
